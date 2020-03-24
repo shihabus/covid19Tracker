08884f558b70c74ea023062e49c298b2238f4323
Shihab US <shihabudheen@bounceshare.com>
2020-03-25 03:55:26 +0530

    Updates

---
+++ precache-manifest.e92069f12ab3551bf142fec1b1095214.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5aa54daa39409f495fdbf32401cfa0c",
+    "url": "/covid19Tracker/index.html"
+  },
+  {
+    "revision": "176c8e1083f43df11418",
+    "url": "/covid19Tracker/static/css/main.26d9c5d5.chunk.css"
+  },
+  {
+    "revision": "14701dca5bebd7f8b9a3",
+    "url": "/covid19Tracker/static/js/2.0abd27b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid19Tracker/static/js/2.0abd27b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "176c8e1083f43df11418",
+    "url": "/covid19Tracker/static/js/main.6aead406.chunk.js"
+  },
+  {
+    "revision": "0140095aed958f614f9d",
+    "url": "/covid19Tracker/static/js/runtime-main.50f47981.js"
+  }
+]);
