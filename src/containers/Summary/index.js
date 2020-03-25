7cee0fced57ef6da1651f26975c3aab26519f33a
Shihab US <shihabudheen@bounceshare.com>
2020-03-25 10:15:12 +0530

    mobile view fixed

--- src/containers/Summary/index.js
+++ src/containers/Summary/index.js
@@ -4,14 +4,14 @@ import styled from "styled-components";
 const Wrapper = styled.div`
   grid-area: content-2;
   height: 100%;
-  display: flex;
-
   margin: 0.5rem 1rem;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
   padding-bottom: 0.5rem;
   color: #7c92a6;
+
+  display: grid;
+  grid-template-rows: repeat(2, 1fr) 2rem;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-areas: "active confirmed" "deaths recovered" "last-updated last-updated";
 
   @media only screen and (min-width: 768px) {
     display: grid;
@@ -22,14 +22,14 @@ const Wrapper = styled.div`
 `;
 
 const Label = styled.div`
-  font-size: 1rem;
+  font-size: 0.75rem;
   @media only screen and (min-width: 768px) {
     font-size: 1.5rem;
   }
 `;
 
 const Count = styled.div`
-  font-size: 2rem;
+  font-size: 1.5rem;
   color: white;
   @media only screen and (min-width: 768px) {
     font-size: 5rem;
@@ -60,19 +60,19 @@ export default function Index(props) {
   } = props.dataSet;
   return (
     <Wrapper>
-      <Section>
+      <Section gridName="active">
         <Label>Total active</Label>
         <Count>{active}</Count>
       </Section>
-      <Section>
+      <Section gridName="confirmed">
         <Label>Total confirmed</Label>
         <Count>{confirmed}</Count>
       </Section>
-      <Section>
+      <Section gridName="deaths">
         <Label>Total deaths</Label>
         <Count>{deaths}</Count>
       </Section>
-      <Section>
+      <Section gridName="recovered">
         <Label>Total recovered</Label>
         <Count>{recovered}</Count>
       </Section>
